0ebf89e45b0da770b8a68eeb12eb4283d79507e5
yoimaral <yalozano9@misena.edu.co>
2021-02-09 13:53:09 -0500

    feat: New file in Users Table database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->boolean('document')->unique();
+            $table->boolean('phone');
             $table->string('email')->unique();
             $table->boolean('is_admin')->default(false);
             $table->timestamp('email_verified_at')->nullable();
