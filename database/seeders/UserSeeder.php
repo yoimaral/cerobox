d8ea3bcc234affff465520ec92b3806660b4f75d
yoimaral <yalozano9@misena.edu.co>
2021-02-09 12:21:58 -0500

    feat: New UserSeeder and create new file table database is admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'Yoimar Lozano';
+        $user->email = 'yoimar@gmail.com';
+        $user->is_admin = true;
+        $user->email_verified_at = now();
+        $user->password = bcrypt('password');
+        $user->save();
+    }
+}
