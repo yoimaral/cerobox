ad39f636f6fc54fb3a3d62180053ae26df3d40a4
yoimaral <yalozano9@misena.edu.co>
2021-02-09 20:01:00 -0500

    feat: New method update users

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -70,9 +70,13 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        //
+        if ($request->trick) {
+            $user->update([
+                'name' => $request->name
+            ]);
+        }
     }
 
     /**
