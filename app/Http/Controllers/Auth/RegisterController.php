6f17fc1f9d98e252a0e64a80766a298721386f13
yoimaral <yalozano9@misena.edu.co>
2021-02-10 00:30:10 -0500

    feat: New file in register user and new file in database table user document and phone

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,8 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'document' => $data['document'],
+            'phone' => $data['phone'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            ///$table->boolean('document')->unique();
-            ///$table->boolean('phone');
-            ///$table->string('observation');
+            $table->boolean('document')->unique();
+            $table->boolean('phone');
+            $table->string('observation');
             $table->string('email')->unique();
             $table->boolean('is_admin')->default(false);
             $table->timestamp('email_verified_at')->nullable();
